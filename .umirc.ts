47ed57d54e8fd2f737c500f1899307992564c297
jian.sun1 <ebiorz@gmail.com>
2022-07-20 11:48:00 +0800

    feat: fta components 接入示例

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'umi';
-
+import modifyWebpack from '@fta/h5-blended-taro3';
+import path from 'path';
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
   routes: [{ path: '/', component: '@/pages/index' }],
+  // 支持 dynamic import
+  dynamicImportSyntax: {},
   fastRefresh: {},
+  chainWebpack(memo, { env, webpack, createCSSRule }) {
+    modifyWebpack(memo);
+    // 非 umi 项目移除该行
+    memo.module.rules.delete('h5-blended-script');
+  },
+  extraBabelIncludes: [path.resolve(__dirname, 'node_modules/@tarojs')],
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
+import { Image, ActionSheet, ActionSheetItem as Item } from '@fta/components';
 import styles from './index.less';
+import React from 'react';
 
 export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+
+      <Image
+        mode="aspectFit"
+        src="https://imagecdn.ymm56.com/ymmfile/static/resource/4a0b4e5f-2f87-48ae-b2c3-b19ee21f8dfe.png"
+      />
+
+      <ActionSheet
+        clickOverlayOnClose
+        isOpened={true}
+        className="demo-action-sheet"
+        containerClassName="demo-action-sheet-container"
+      >
+        <Item hint="辅助文案辅助文案">操作1</Item>
+        <Item>操作2</Item>
+        <Item>操作3</Item>
+        <Item left>操作4左对齐</Item>
+        <Item noBorder left>
+          操作5 左对齐
+        </Item>
+      </ActionSheet>
     </div>
   );
 }
