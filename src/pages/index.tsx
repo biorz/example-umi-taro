7eb58373940ae74167b2f7ad87f66c9060055f93
biorz <ebiorz@gmail.com>
2022-09-27 09:39:06 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,7 +60,7 @@ export default function IndexPage() {
           // setTimeout(show, 100)
         }}
         // enableCheckAll={[true, false]}
-        containerStyle={{ height: '350px' }}
+        containerStyle={{ height: '380px' }}
         onConfirm={onConfirm}
         onExceed={onExceed}
         onChange={(_: any[], selected: any[]) => {
