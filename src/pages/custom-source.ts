b3e0198c193051afced4cb98da4498ec422dfdbc
jian.sun1 <ebiorz@gmail.com>
2022-09-27 17:22:27 +0800

    feat: 使用 address-picker 作为示例

---
+++ src/pages/custom-source.ts
@@ -0,0 +1,71 @@
+export const customSource = [
+  {
+    code: 1,
+    id: 1,
+    name: '江苏省',
+    shortName: '江苏省',
+    children: [
+      {
+        code: 11,
+        id: 11,
+        name: '南京市',
+        shortName: '南京市',
+        children: [
+          { code: 111, id: 111, name: '宣武区', shortName: '宣武区' },
+          { code: 112, id: 112, name: '秦淮区', shortName: '秦淮区' },
+          { code: 112, id: 112, name: '建邺区', shortName: '建邺区' },
+          { code: 111, id: 111, name: '宣武区', shortName: '宣武区' },
+          { code: 112, id: 112, name: '秦淮区', shortName: '秦淮区' },
+          { code: 112, id: 112, name: '建邺区', shortName: '建邺区' },
+          { code: 111, id: 111, name: '宣武区', shortName: '宣武区' },
+          { code: 112, id: 112, name: '秦淮区', shortName: '秦淮区' },
+          { code: 112, id: 112, name: '建邺区', shortName: '建邺区' },
+          { code: 111, id: 111, name: '宣武区', shortName: '宣武区' },
+          { code: 112, id: 112, name: '秦淮区', shortName: '秦淮区' },
+          { code: 112, id: 112, name: '建邺区', shortName: '建邺区' },
+          { code: 111, id: 111, name: '宣武区', shortName: '宣武区' },
+          { code: 112, id: 112, name: '秦淮区', shortName: '秦淮区' },
+          { code: 112, id: 112, name: '建邺区', shortName: '建邺区' },
+        ],
+      },
+      {
+        code: 12,
+        id: 12,
+        name: '无锡市',
+        shortName: '无锡市',
+        children: [
+          { code: 121, id: 121, name: '凉席', shortName: '凉席' },
+          { code: 122, id: 122, name: '锡山', shortName: '锡山' },
+        ],
+      },
+    ],
+  },
+  {
+    code: 2,
+    id: 2,
+    name: '浙江省',
+    shortName: '浙江省',
+    children: [
+      {
+        code: 21,
+        id: 21,
+        name: '杭州市',
+        shortName: '杭州市',
+        children: [
+          { code: 211, id: 211, name: '钱塘', shortName: '钱塘' },
+          { code: 212, id: 212, name: '有名市', shortName: '有名市' },
+        ],
+      },
+      {
+        code: 22,
+        id: 22,
+        name: '宁波市',
+        shortName: '宁波市',
+        children: [
+          { code: 221, id: 221, name: '夕张2市', shortName: '夕张2市' },
+          { code: 222, id: 222, name: '美呗2市', shortName: '美呗2市' },
+        ],
+      },
+    ],
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,81 @@
-import { Image, ActionSheet, ActionSheetItem as Item } from '@fta/components';
-import styles from './index.less';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import AddressPicker from '@fta/components-address-picker';
+import { Button } from '@tarojs/components';
+import { customSource as options } from './custom-source';
+
+const AddressPickerParams = {
+  showSearch: true,
+  showResult: true,
+  multiple: true,
+  useCustom: true,
+  showCount: true,
+  title: '请选择装卸货地址',
+  placeholder: 'input搜索地址喔',
+  emptyHint: '搜到个锤子🔨',
+  limitHint: '别给我选择超过3个',
+  limit: 3,
+  depth: 3,
+  // defaultValue: [130100, 540000],
+  enableCheckAll: [true, true],
+  confirmText: '完成',
+  cancelText: '关闭',
+};
 
 export default function IndexPage() {
+  const ref = useRef<any>([]);
+  const [resultRef, setResult] = useState<any>([]);
+  const [propsString, setPropsString] = useState(
+    JSON.stringify(AddressPickerParams, null, 2),
+  );
+  const [isOpened, toggle] = useState(true);
+
+  const selectBaseProps = safeResolve(propsString);
+  const close = () => toggle(false);
+
+  const show = () => {
+    if (!selectBaseProps) {
+      window.alert('参数无法序列化为JSON对象');
+      return;
+    }
+    toggle(true);
+  };
+  const onConfirm = (selected: any) => {
+    console.log('选择的选项', selected);
+    close();
+  };
+  const onExceed = () => {
+    window.alert('最多选择3项');
+  };
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
-
-      <Image
-        mode="aspectFit"
-        src="https://imagecdn.ymm56.com/ymmfile/static/resource/4a0b4e5f-2f87-48ae-b2c3-b19ee21f8dfe.png"
+      <Button onClick={show}>打开</Button>
+      <AddressPicker
+        ref={ref}
+        {...selectBaseProps}
+        isOpened={isOpened}
+        onClose={close}
+        onCancel={close}
+        onFetch={(options: any) => {
+          console.log('==获取到数据源==', options);
+          // setTimeout(show, 100)
+        }}
+        // enableCheckAll={[true, false]}
+        onConfirm={onConfirm}
+        onExceed={onExceed}
+        onChange={(_: any[], selected: any[]) => {
+          console.log('选项改变', selected);
+          setResult(selected);
+        }}
+        options={options}
       />
-
-      <ActionSheet
-        clickOverlayOnClose
-        isOpened={true}
-        className="demo-action-sheet"
-        containerClassName="demo-action-sheet-container"
-      >
-        <Item hint="辅助文案辅助文案">操作1</Item>
-        <Item>操作2</Item>
-        <Item>操作3</Item>
-        <Item left>操作4左对齐</Item>
-        <Item noBorder left>
-          操作5 左对齐
-        </Item>
-      </ActionSheet>
     </div>
   );
 }
+
+const safeResolve = (jsonString: string) => {
+  try {
+    return JSON.parse(jsonString);
+  } catch (error) {
+    return null;
+  }
+};
